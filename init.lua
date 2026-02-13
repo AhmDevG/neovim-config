fb7a5947cfb9cd9d95dbf6b3408eb19c79db4d76
ma9737149 <ahmedashraf3695@gmail.com>
2026-02-13 18:01:41 +0200

    make the backtround color of the theme to be black

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
 require("config.sets")
 require("config.remaps")
 
+local colors = require("config.colors")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "rose-pine",
+    callback = function()
+        colors.ColorMyPencils()
+    end,
+})
 
---
+++ lua/config/colors.lua
@@ -0,0 +1,13 @@
+-- @diagnostic disable: undefined-global
+local M = {}
+
+function M.ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
+
+end
+
+return M
