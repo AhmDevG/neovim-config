33b01ca2ae57518b6fe13e0c3fe70a38903edcd4
ma9737149 <ahmedashraf3695@gmail.com>
2026-02-13 18:10:01 +0200

    added some cool remaps for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,4 +6,12 @@ vim.keymap.set('n', '<C-p>', builtin.git_files , {})
 vim.keymap.set("n" , "<leader>ps" , function ()
 	builtin.grep_string({search = vim.fn.input("Grep > ")})
 end)
+vim.keymap.set('n','<leader>pws' , function()
+    builtin.grep_string({ search = vim.fn.expand("<cword>") })
+end)
 
+vim.keymap.set('n','<leader>pWs' , function()
+    builtin.grep_string({ search = vim.fn.expand("<cWORD>") })
+end)
+
+vim.keymap.set('n','<leader>vh' , builtin.help_tags , {})
