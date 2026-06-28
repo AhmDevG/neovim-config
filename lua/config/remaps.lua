883eb4f6e44dc39e3ad4aa6fc502a85b4bf0f6e6
ma9737149 <ahmedashraf3695@gmail.com>
2026-06-28 04:51:26 +0300

    changed the quick list

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -105,5 +105,5 @@ keymap("n", "<leader>gl", "<cmd>diffget //3<CR>", { desc = "Take Right" })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- quick fix list remaps
-vim.keymap.set("n", "<leader>n", "<cmd>cnext<CR>", {desc = "Navigated to the next element in the quick fix list"})
-vim.keymap.set("n", "<leader>p", "<cmd>cprevious<CR>", {desc = "Navigated to the prev element in the quick fix list"})
+vim.keymap.set("n", "[n", "<cmd>cnext<CR>", {desc = "Navigated to the next element in the quick fix list"})
+vim.keymap.set("n", "]p", "<cmd>cprevious<CR>", {desc = "Navigated to the prev element in the quick fix list"})
